61b79cce9d842c37e378fb377c88ddf3280c1138
nhatduy209 <nhatduy20000@gmail.com>
2020-12-03 21:13:51 +0700

    commit chartJS

--- QLyKhachSan/QLyKhachSan/Areas/Admin/Controllers/HomeAdminController.cs
+++ QLyKhachSan/QLyKhachSan/Areas/Admin/Controllers/HomeAdminController.cs
@@ -13,5 +13,10 @@ namespace QLyKhachSan.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public ActionResult ChartBar()
+        {
+            return View();
+        }
     }
 }
